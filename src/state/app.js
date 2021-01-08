0587e1ec4b1be774caca6432d22452ecff9b4363
TopTecShare <eestimorol.ice9208g@gmail.com>
2021-01-09 04:15:30 +0800

    Update app.js

--- src/state/app.js
+++ src/state/app.js
@@ -5,15 +5,26 @@ import { initNear } from './near';
 const initialState = {
 	app: {
 		mounted: false,
+		tab: 1,
+		sort: 2,
+		filter: 1,
 	},
 	near: {
 		initialized: false,
 	},
 };
+let snackTimeout;
 
 export const { appStore, AppProvider } = State(initialState, 'app');
 
 export const onAppMount = () => async ({ update, getState, dispatch }) => {
 	update('app', { mounted: true });
 	dispatch(initNear());
+};
+
+export const snackAttack = (msg) => async ({ update, getState, dispatch }) => {
+	console.log('Snacking on:', msg);
+	update('app.snack', msg);
+	if (snackTimeout) clearTimeout(snackTimeout);
+	snackTimeout = setTimeout(() => update('app.snack', null), 3000);
 };
