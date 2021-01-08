96a1b3b6833a12161d8488c69c417a7a21293c85
TopTecShare <eestimorol.ice9208g@gmail.com>
2021-01-09 04:09:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { appStore, onAppMount } from './state/app';
 
 import { Wallet } from './components/Wallet';
 import { Contract } from './components/Contract';
-import { Keys } from './components/Keys';
 import { Gallery } from './components/Gallery';
 
 import Avatar from 'url:./img/avatar.jpg';
@@ -15,10 +14,8 @@ import './App.scss';
 const App = () => {
 	const { state, dispatch, update } = useContext(appStore);
 
-    console.log(state)
-    
-	const { near, wallet, contractAccount, account, localKeys, loading } = state;
-    
+	const { app, app: {tab, snack}, near, wallet, contractAccount, account, loading } = state;
+
 	const [profile, setProfile] = useState(false);
 
 	const onMount = () => {
@@ -26,65 +23,66 @@ const App = () => {
 	};
 	useEffect(onMount, []);
 
-	const signedIn = ((wallet && wallet.signedIn) || (localKeys && localKeys.signedIn));
-	let accountId = '';
-	if (signedIn) {
-		accountId = account ? account.accountId : <span>Guest Account<br />{localKeys.accessAccountId.split('.')[0]}</span>;
-	}
+
+	const signedIn = ((wallet && wallet.signedIn));
 
 	if (profile && !signedIn) {
 		setProfile(false);
 	}
-    
+
 	return <>
 		{ loading && <div className="loading">
 			<img src={NearLogo} />
 		</div>
 		}
-        
+		{
+			snack &&
+			<div className="snack">
+				{snack}
+			</div>
+		}
+
+		<div className="background"></div>
+
 		<div id="menu">
 			<div>
-				<div>
-					<img style={{ opacity: signedIn ? 1 : 0.25 }} src={Avatar} 
-						onClick={() => setProfile(!profile)}
-					/>
-				</div>
-				<div>
-					{ !signedIn ? <Wallet {...{ wallet }} /> : accountId }
-				</div>
+				<img style={{ opacity: signedIn ? 1 : 0.25 }} src={Avatar}
+					onClick={() => setProfile(!profile)}
+				/>
 			</div>
+			<div>
+				{!signedIn ? <Wallet {...{ wallet }} /> : account.accountId}
+			</div>
+			{
+				profile && signedIn && <div id="profile">
+					<div>
+						{
+							wallet && wallet.signedIn && <Wallet {...{ wallet, account, update, dispatch, handleClose: () => setProfile(false) }} />
+						}
+					</div>
+				</div>
+			}
 		</div>
 
+
 		{
-			profile && signedIn && <div id="profile">
-				<div>
-					{
-						wallet && wallet.signedIn && <Wallet {...{ wallet, account, update, dispatch, handleClose: () => setProfile(false) }} />
-					}
-					{
-						localKeys && localKeys.signedIn && <Keys {...{ near, update, localKeys }} />
-					}
-				</div>
+			signedIn && <div id="tabs">
+				<div onClick={() => update('app.tab', 1)} style={{ background: tab === 1 ? '#fed' : '' }}>Market</div>
+				<div onClick={() => update('app.tab', 2)} style={{ background: tab === 2 ? '#fed' : '' }}>My NFTs</div>
+				<div onClick={() => update('app.tab', 3)} style={{ background: tab === 3 ? '#fed' : '' }}>Mint</div>
 			</div>
 		}
 
-		{ !signedIn &&
-            <div id="guest">
-            	<>
-            		<Keys {...{ near, update, localKeys }} />
-            	</>
-            </div>
-		}
-		{ signedIn &&
-            <div id="contract">
-            	{
-            		signedIn &&
-                    <Contract {...{ near, update, localKeys, wallet, account }} />
-            	}
-            </div>
+		{ signedIn && tab === 3 &&
+			<div id="contract">
+				{
+					signedIn &&
+					<Contract {...{ near, update, wallet, account }} />
+				}
+			</div>
 		}
 		<div id="gallery">
-			<Gallery {...{ near, signedIn, contractAccount, account, localKeys, loading, update }} />
+			<Gallery {...{ app, update, loading, contractAccount, account, dispatch }} />
 		</div>
 	</>;
 };
